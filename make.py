fbd5a328e70e93c104086869b215161027bc8bc2
wotsushi <wotsushi@gmail.com>
2019-04-19 22:19:31 +0900

    Add make scripts

---
+++ make.py
@@ -0,0 +1,29 @@
+import yaml
+import json
+import importlib
+
+
+if __name__ == "__main__":
+    with open('langs.yml') as langs_yml:
+        langs = yaml.load(langs_yml, Loader=yaml.FullLoader)
+    for lang in langs:
+        lang_name = lang['lang']
+        ext = lang['ext']
+        with open(f'{lang_name}/{lang_name}.yml') as snippet_srcs:
+            snippet_list = yaml.load(snippet_srcs, Loader=yaml.FullLoader)
+        parser = importlib.import_module(f'{lang_name}.parser').parse
+        with open(f'{lang_name}.json', 'w') as snippets_json:
+            json.dump(
+                {
+                    snippet['name']: {
+                        'prefix': snippet['prefix'],
+                        'description': snippet['description'],
+                        'body': parser(
+                            f'{lang_name}/snippets/{snippet["name"]}.{ext}'
+                        )
+                    }
+                    for snippet in snippet_list
+                },
+                snippets_json,
+                indent=2
+            )
