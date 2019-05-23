532126a21c71b0d19859095066f6244f7af23b14
wotsushi <wotsushi@gmail.com>
2019-05-24 00:06:24 +0900

    Fix python parser

--- python/parser.py
+++ python/parser.py
@@ -29,7 +29,7 @@ def parse(src_path: str) -> List[str]:
                 enumerate(
                     iterable=dict.fromkeys(
                         re.findall(
-                            pattern=r'\W_(\w+)',
+                            pattern=r'\W_([a-zA-Z0-9]+)',
                             string=src
                         )
                     ),
