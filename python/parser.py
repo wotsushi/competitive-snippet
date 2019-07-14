2eae86ce7db9ab9968e4f486e056dec60927710d
wotsushi <wotsushi@gmail.com>
2019-07-14 18:50:30 +0900

    Fix build error

--- python/parser.py
+++ python/parser.py
@@ -1,41 +1,39 @@
-import inspect
-import importlib
-import os
 import re
-from itertools import takewhile
+from itertools import takewhile, dropwhile
 from functools import reduce
 from typing import List
 
 
 def parse(src_path: str) -> List[str]:
-    src_root, _ = os.path.splitext(src_path)
-    _, src = inspect.getsource(
-        importlib.import_module(
-            src_root.replace('/', '.')
-        ).code
-    ).split(
-        sep='\n',
-        maxsplit=1
-    )
+    with open(src_path) as src_file:
+        src = '\n'.join(
+            [
+                line.rstrip()
+                for line in takewhile(
+                    lambda line: not line.startswith('    return'),
+                    dropwhile(
+                        lambda line: not line.startswith('def code'),
+                        src_file
+                    )
+                )
+            ][1:]
+        )
     return [
         line[4:]
-        for line in takewhile(
-            lambda line: not line.startswith('    return'),
-            reduce(
-                lambda s, v: s.replace(
-                    f'_{v[1]}',
-                    f'${{{v[0]}:{v[1]}}}'
+        for line in reduce(
+            lambda s, v: s.replace(
+                f'_{v[1]}',
+                f'${{{v[0]}:{v[1]}}}'
+            ),
+            enumerate(
+                iterable=dict.fromkeys(
+                    re.findall(
+                        pattern=r'\W_([a-zA-Z0-9]+)',
+                        string=src
+                    )
                 ),
-                enumerate(
-                    iterable=dict.fromkeys(
-                        re.findall(
-                            pattern=r'\W_([a-zA-Z0-9]+)',
-                            string=src
-                        )
-                    ),
-                    start=1
-                ),
-                src
-            ).split('\n')
-        )
+                start=1
+            ),
+            src
+        ).split('\n')
     ]
