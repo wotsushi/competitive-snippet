a12b2cda6e362414650a4ab6e915b4c89e90fb00
wotsushi <wotsushi@gmail.com>
2019-07-15 00:54:43 +0900

    Speed up segment tree

--- python/snippets/datastructure/segment_tree.py
+++ python/snippets/datastructure/segment_tree.py
@@ -1,41 +1,29 @@
 def code():
     class SegmentTree:
         def __init__(self, n, f, e):
-            def v(i, j):
-                return (
-                    [i, j, e] if j == i + 1 else
-                    [
-                        i,
-                        j,
-                        v(i, (i + j) // 2),
-                        v((i + j) // 2, j),
-                        e
-                    ]
-                )
             self.n = 2**((n - 1).bit_length())
-            self.r = v(1, self.n + 1)
             self.f = f
             self.e = e
+            self.d = [e for _ in range(2 * self.n)]
 
         def __setitem__(self, key, value):
-            def u(v):
-                if v[1] == v[0] + 1:
-                    v[-1] = value
-                else:
-                    u(
-                        v[2] if key < (v[0] + v[1]) // 2 else
-                        v[3]
-                    )
-                    v[-1] = self.f(v[2][-1], v[3][-1])
-            u(self.r)
+            key += self.n - 1
+            self.d[key] = value
+            while key > 1:
+                key //= 2
+                self.d[key] = self.f(self.d[2 * key], self.d[2 * key + 1])
 
         def __getitem__(self, key):
-            def q(v):
-                return (
-                    self.e if v[0] >= key.stop or v[1] <= key.start else
-                    v[-1] if key.start <= v[0] <= v[1] <= key.stop else
-                    self.f(q(v[2]), q(v[3]))
+            return self.q(key, 1, 1, self.n + 1)
+
+        def q(self, key, i, left, right):
+            return (
+                self.e if left >= key.stop or right <= key.start else
+                self.d[i] if key.start <= left <= right <= key.stop else
+                self.f(
+                    self.q(key, 2 * i, left, (left + right) // 2),
+                    self.q(key, 2 * i + 1, (left + right) // 2, right)
                 )
-            return q(self.r)
+            )
 
     return SegmentTree
