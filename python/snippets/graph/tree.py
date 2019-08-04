d1328bbad2a40fa93bfc4bb3b1fd589a5d998828
wotsushi <wotsushi@gmail.com>
2019-08-04 20:24:41 +0900

    Change tree's output

--- python/snippets/graph/tree.py
+++ python/snippets/graph/tree.py
@@ -1,14 +1,16 @@
 def code():
     def tree(G, r):
-        res = [None for _ in range(len(G))]
+        p = [i for i in range(len(G))]
+        c = [set() for _ in range(len(G))]
         q = []
-        res[r] = (r, G[r])
+        c[r] = G[r]
         q.append(r)
         while q:
             i = q.pop()
             for j in G[i]:
-                if res[j] is None:
-                    res[j] = (i, G[j] - {i})
+                if j != p[i]:
+                    p[j] = i
+                    c[j] = G[j] - {i}
                     q.append(j)
-        return res
+        return p, c
     return tree
