68fd010ebad6fa688db95c6a15bdca4a37456522
wotsushi <wotsushi@gmail.com>
2019-09-05 22:30:20 +0900

    Add GCD

---
+++ snippets/math/gcd/gcd.rs
@@ -0,0 +1,30 @@
+#![allow(non_snake_case)]
+#![allow(unused_variables)]
+
+// https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/1/ALDS1_1_B
+
+fn main() {
+    let (x, y): (i64, i64) = {
+        let mut line: String = String::new();
+        std::io::stdin().read_line(&mut line).unwrap();
+        let mut iter = line.split_whitespace();
+        (
+            iter.next().unwrap().parse().unwrap(),
+            iter.next().unwrap().parse().unwrap()
+        )
+    };
+
+    // snip
+    fn gcd(a: i64, b: i64) -> i64 {
+        if b == 0 {
+            a
+        } else {
+            gcd(b, a % b)
+        }
+    }
+    //
+
+    let ans = gcd(x, y);
+
+    println!("{}", ans);
+}
