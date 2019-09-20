dffc92e0932d97891712ab45589b214f98330be6
wotsushi <wotsushi@gmail.com>
2019-09-20 22:44:46 +0900

    Add template snippet

---
+++ snippets/template/template/template.rs
@@ -0,0 +1,13 @@
+// snip
+    #![allow(non_snake_case)]
+    #![allow(unused_variables)]
+    #![allow(dead_code)]
+
+    fn main() {
+
+
+        let ans =
+
+        println!("{}", ans);
+    }
+// snip
