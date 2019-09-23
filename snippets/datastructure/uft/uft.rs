77185f85ddb26af1973e8b01253fcb0ea28c9661
wotsushi <wotsushi@gmail.com>
2019-09-23 11:01:54 +0900

    Move snippet code(uft) into main func

--- snippets/datastructure/uft/uft.rs
+++ snippets/datastructure/uft/uft.rs
@@ -4,49 +4,6 @@
 
 // https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/1/DSL_1_A
 
-// snip
-struct UnionFindTree {
-    parent: Vec<usize>,
-    size: Vec<i64>
-}
-
-impl UnionFindTree {
-    fn new(n: usize) -> UnionFindTree {
-        UnionFindTree {
-            parent: (0..(n + 1)).collect(),
-            size: vec![1; n + 1]
-        }
-    }
-    fn find(&mut self, x: usize) -> usize {
-        if self.parent[x] != x {
-            let parent_x = self.parent[x];
-            self.parent[x] = self.find(parent_x);
-        }
-        self.parent[x]
-    }
-    fn same(&mut self, x: usize, y: usize) -> bool {
-        self.find(x) == self.find(y)
-    }
-    fn size(&mut self, x: usize) -> i64 {
-        let i = self.find(x);
-        self.size[i]
-    }
-    fn union(&mut self, x: usize, y: usize) {
-        let parent_x = self.find(x);
-        let parent_y = self.find(y);
-        if !self.same(x, y) {
-            if self.size[parent_x] < self.size[parent_y] {
-                self.size[parent_y] += self.size[parent_x];
-                self.parent[parent_x] = parent_y;
-            } else {
-                self.size[parent_x] += self.size[parent_y];
-                self.parent[parent_y] = parent_x;
-            }
-        }
-    }
-}
-// snip
-
 fn main() {
     let (n, q): (usize, usize) = {
         let mut line: String = String::new();
@@ -69,6 +26,49 @@ fn main() {
         }
         (com, x, y)
     };
+
+    // snip
+    struct UnionFindTree {
+        parent: Vec<usize>,
+        size: Vec<i64>
+    }
+
+    impl UnionFindTree {
+        fn new(n: usize) -> UnionFindTree {
+            UnionFindTree {
+                parent: (0..(n + 1)).collect(),
+                size: vec![1; n + 1]
+            }
+        }
+        fn find(&mut self, x: usize) -> usize {
+            if self.parent[x] != x {
+                let parent_x = self.parent[x];
+                self.parent[x] = self.find(parent_x);
+            }
+            self.parent[x]
+        }
+        fn same(&mut self, x: usize, y: usize) -> bool {
+            self.find(x) == self.find(y)
+        }
+        fn size(&mut self, x: usize) -> i64 {
+            let i = self.find(x);
+            self.size[i]
+        }
+        fn union(&mut self, x: usize, y: usize) {
+            let parent_x = self.find(x);
+            let parent_y = self.find(y);
+            if !self.same(x, y) {
+                if self.size[parent_x] < self.size[parent_y] {
+                    self.size[parent_y] += self.size[parent_x];
+                    self.parent[parent_x] = parent_y;
+                } else {
+                    self.size[parent_x] += self.size[parent_y];
+                    self.parent[parent_y] = parent_x;
+                }
+            }
+        }
+    }
+    // snip
 
     let mut uft = UnionFindTree::new(n - 1);
     let ans = {
