fbf9d9585136894663968fae9730ef521d000027
wotsushi <wotsushi@gmail.com>
2019-12-04 22:46:33 +0900

    Add ascii_lowercase

---
+++ snippets/string/ascii_lowercase/ascii_lowercase.rs
@@ -0,0 +1,9 @@
+#![allow(non_snake_case)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
+fn main() {
+    // snip
+    let ascii_lowercase = "abcdefghijklmnopqrstuvwxyz".chars().collect::<Vec<_>>();
+    // snip
+}
