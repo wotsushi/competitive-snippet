aaa7571b395e899c05486db026cec758f52f8a36
wotsushi <wotsushi@gmail.com>
2019-12-05 20:29:40 +0900

    Add ascii_uppercase

---
+++ snippets/string/ascii_uppercase/ascii_uppercase.rs
@@ -0,0 +1,9 @@
+#![allow(non_snake_case)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
+fn main() {
+    // snip
+    let ascii_uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".chars().collect::<Vec<_>>();
+    // snip
+}
